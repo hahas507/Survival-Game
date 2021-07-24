fe2a09a4e1badef31ba3bac9df28d6ac15672a0a
hahas507 <aq12361@gmail.com>
2021-07-24 20:38:02 +0900

    0.1 기본 캐릭터 움직임

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+
+    private float currentCameraRotationX = 0f;
+
+    [SerializeField]
+    private Camera theCamera;
+
+    private Rigidbody myRigid;
+
+    private void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    private void Move()
+    {
+        float moveDirX = Input.GetAxis("Horizontal");
+        float moveDirZ = Input.GetAxis("Vertical");
+
+        Vector3 moveHorizontal = transform.right * moveDirX;
+        Vector3 moveVertical = transform.forward * moveDirZ;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    private void CameraRotation()
+    {
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        float cameraRotationX = xRotation * lookSensitivity;
+        currentCameraRotationX -= cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+    private void CharacterRotation()
+    {
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 characterRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(characterRotationY));
+    }
+}
