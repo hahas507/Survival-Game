7a65376836b1b801f1d51455a7e1cffdb980857d
hahas507 <aq12361@gmail.com>
2021-07-24 21:13:40 +0900

    V03

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private float runSpeed;
+
+    [SerializeField]
     private float walkSpeed;
+
+    [SerializeField]
+    private float crouchSpeed;
+
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    private bool isRun = false;
+    private bool isGround = true;
+    private bool isCrouch = false;
+
+    //앉았을때 얼마나 앉을지
+    [SerializeField]
+    private float crouchPosY;
+
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    private CapsuleCollider capsuleCollider;
 
     [SerializeField]
     private float lookSensitivity;
@@ -23,14 +47,90 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
     }
 
     private void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
+    }
+
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        theCamera.transform.localPosition = new Vector3(theCamera.transform.localPosition.x, applyCrouchPosY, theCamera.transform.localPosition.z);
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = runSpeed;
     }
 
     private void Move()
@@ -41,7 +141,7 @@ public class PlayerController : MonoBehaviour
         Vector3 moveHorizontal = transform.right * moveDirX;
         Vector3 moveVertical = transform.forward * moveDirZ;
 
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + velocity * Time.deltaTime);
     }
