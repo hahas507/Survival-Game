d686390bfa1685aa1d97352e845ab38cc8d29606
hahas507 <aq12361@gmail.com>
2021-07-25 13:26:09 +0900

    0.3 ~ 0.4
    지형 제작과 팔 구현

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    //현재 장착된 Hand형 타입 무기
+    [SerializeField]
+    private Hand currentHand;
+
+    private bool isAttack = false;
+    private bool isSwing = false;
+
+    private RaycastHit hitInfo;
+
+    private void Update()
+    {
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!isAttack)
+            {
+                //코루틴 실행
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        currentHand.anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true; //공격 활성화 시점
+
+        StartCoroutine(HitCoroutine());
+
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+
+        isAttack = false;
+    }
+
+    private IEnumerator HitCoroutine()
+    {
+        while (isSwing)
+        {
+            if (CheckObject())
+            {
+                isSwing = false;
+                Debug.Log(hitInfo.transform.name);
+            }
+            yield return null;
+        }
+    }
+
+    private bool CheckObject()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range))
+        {
+            return true;
+        }
+        return false;
+    }
+}
