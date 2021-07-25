6a367cc4f7d24147657d36fc5d3989e19711adea
hahas507 <aq12361@gmail.com>
2021-07-25 14:36:29 +0900

    1.1
    총 구현과 총구섬광

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public string gunName;
+
+    public float range;
+    public float accuracy;
+    public float fireRate;
+    public float reloadTime;
+
+    public int damage;
+
+    public int reloadBulletCount;
+    public int currentBulletCount;
+    public int maxBulletCount;
+    public int carryBulletCount;
+
+    public float retroAction; // 반동세기
+    public float retroActionFineSightForce; //정조준 반동세기
+
+    public Vector3 fineSightOriginPos;
+
+    public Animator anim;
+    public ParticleSystem muzzleFlash;
+    public AudioClip fireSound;
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GunFireRateCalc();
+        TryFire();
+    }
+
+    private void TryFire()
+    {
+        if (Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        PlaySE(currentGun.fireSound);
+        currentGun.muzzleFlash.Play();
+        Debug.Log("Gun fired");
+    }
+
+    private void GunFireRateCalc()
+    {
+        if (currentFireRate > 0)
+        {
+            currentFireRate -= Time.deltaTime;
+        }
+    }
+
+    private void PlaySE(AudioClip _clip)
+    {
+        audioSource.clip = _clip;
+        audioSource.Play();
+    }
+}
