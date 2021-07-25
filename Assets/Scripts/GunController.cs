59cc61689202cd1c915fb32520a7c449f160aadf
hahas507 <aq12361@gmail.com>
2021-07-25 20:42:50 +0900

    1.4
    HUD구현

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -234,4 +234,9 @@ public class GunController : MonoBehaviour
         audioSource.clip = _clip;
         audioSource.Play();
     }
+
+    public Gun GetGun()
+    {
+        return currentGun;
+    }
 }
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    //필요한 컴포넌트
+    [SerializeField]
+    private GunController theGunController;
+
+    private Gun currentGun;
+
+    //HUD on/off
+    [SerializeField]
+    private GameObject go_BulletHUD;
+
+    //총알 개수 텍스트에 반영
+    [SerializeField]
+    private Text[] textBullet;
+
+    private void Update()
+    {
+        CheckBullet();
+    }
+
+    private void CheckBullet()
+    {
+        currentGun = theGunController.GetGun();
+        textBullet[0].text = currentGun.carryBulletCount.ToString();
+        textBullet[1].text = currentGun.reloadBulletCount.ToString();
+        textBullet[2].text = currentGun.currentBulletCount.ToString();
+    }
+}
