31a679b7abf52833a8389a68622e6fc6bc003703
hahas507 <aq12361@gmail.com>
2021-08-01 16:51:37 +0900

    Crafting bonfire
    Added crafting menu.
    Player can craft objects by clicking the image.

---
+++ Assets/Scripts/UI Scripts/CraftManual.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Craft
+{
+    public string craftName;
+    public GameObject go_Prefab;
+    public GameObject go_PreviewPrefab;
+}
+
+public class CraftManual : MonoBehaviour
+{
+    private bool isActivated = false;
+    private bool isPreviewActivated = false;
+
+    [SerializeField]
+    private GameObject go_BaseUI;
+
+    [SerializeField]
+    private Craft[] craft_fire; //taps for bonfire
+
+    private GameObject go_Preview;
+    private GameObject go_Prefab; // the actual object to spawn.
+
+    [SerializeField]
+    private Transform tf_Player;
+
+    private RaycastHit hitInfo;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private float range;
+
+    public void SlotClick(int _slotNum)
+    {
+        go_Preview = Instantiate(craft_fire[_slotNum].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
+        go_Prefab = craft_fire[_slotNum].go_Prefab;
+        isPreviewActivated = true;
+        go_BaseUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !isPreviewActivated)
+        {
+            Window();
+        }
+
+        if (isPreviewActivated)
+        {
+            PreviewPositionUpdate();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Build();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cancel();
+        }
+    }
+
+    private void Build()
+    {
+        if (isPreviewActivated)
+        {
+            Instantiate(go_Prefab, hitInfo.point, Quaternion.identity);
+            Destroy(go_Preview);
+            isActivated = false;
+            isPreviewActivated = false;
+            go_Preview = null;
+            go_Prefab = null;
+        }
+    }
+
+    private void PreviewPositionUpdate()
+    {
+        if (Physics.Raycast(tf_Player.position, tf_Player.forward, out hitInfo, range, layerMask)) ;
+        {
+            Vector3 _location = hitInfo.point;
+            go_Preview.transform.position = _location;
+        }
+    }
+
+    private void Cancel()
+    {
+        if (isPreviewActivated)
+        {
+            Destroy(go_Preview);
+        }
+        isActivated = false;
+        isPreviewActivated = false;
+        go_Preview = null;
+        go_Prefab = null;
+
+        go_BaseUI.SetActive(false);
+    }
+
+    private void Window()
+    {
+        if (!isActivated)
+        {
+            OpenWindow();
+        }
+        else
+        {
+            CloseWindow();
+        }
+    }
+
+    private void OpenWindow()
+    {
+        isActivated = true;
+        go_BaseUI.SetActive(true);
+    }
+
+    private void CloseWindow()
+    {
+        isActivated = false;
+        go_BaseUI.SetActive(false);
+    }
+}
