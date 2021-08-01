99e2ef461c2ac851e93fb8911f81de6d466449c1
hahas507 <aq12361@gmail.com>
2021-08-02 01:10:03 +0900

    Simple title menu
    Using scene manager library, player can start GameStage scene by clicking START button. LOAD button has no fuction yet.

---
+++ Assets/Scripts/UI Scripts/Title.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName = "GameStage";
+
+    public void ClickStart()
+    {
+        Debug.Log("loading");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ClickLoad()
+    {
+        Debug.Log("load");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("exit");
+    }
+}
